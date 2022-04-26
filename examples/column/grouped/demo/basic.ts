3e8611ae884e56e1a0a53d46f2e3ef4f736e26fb
react-antd <xi378533367@qq.com>
2022-04-26 14:58:57 +0800

    Update basic.ts

--- examples/column/grouped/demo/basic.ts
+++ examples/column/grouped/demo/basic.ts
@@ -95,7 +95,7 @@ const stackedColumnPlot = new Column('container', {
   // marginRatio: 0.1,
   label: {
     // 可手动配置 label 数据标签位置
-    position: 'middle', // 'top', 'middle', 'bottom'
+    position: 'top', // 'top', 'middle', 'bottom'
     // 可配置附加的布局方法
     layout: [
       // 柱形图数据标签位置自动调整
